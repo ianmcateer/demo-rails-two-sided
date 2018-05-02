4387e07aec355a3c6d6953c6d70d722746a75235
ianmcateer <ian-mcateer55@hotmail.com>
2018-05-02 23:22:43 +1000

    fixed js

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,20 +1,22 @@
 class ListingsController < ApplicationController
-  before_action :set_listing, only: %i[show edit update destroy]
-  before_action :authenticate_user!, only: %i[new create edit update destroy]
-  before_action :check_user, only: %i[edit update destroy]
-  # GET /listings
-  # GET /listings.json
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
+
   def seller
     @listings = Listing.where(user: current_user).order("created_at DESC")
   end
 
+  # GET /listings
+  # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.order("created_at DESC")
   end
 
   # GET /listings/1
   # GET /listings/1.json
-  def show; end
+  def show
+  end
 
   # GET /listings/new
   def new
@@ -22,7 +24,8 @@ class ListingsController < ApplicationController
   end
 
   # GET /listings/1/edit
-  def edit; end
+  def edit
+  end
 
   # POST /listings
   # POST /listings.json
@@ -30,12 +33,26 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
 
+    if current_user.recipient.blank?
+      Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token = params[:stripeToken]
+
+      recipient = Stripe::Recipient.create(
+        :name => current_user.name,
+        :type => "individual",
+        :bank_account => token
+        )
+
+      current_user.recipient = recipient.id
+      current_user.save
+    end
+
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+        format.json { render action: 'show', status: :created, location: @listing }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -47,9 +64,9 @@ class ListingsController < ApplicationController
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -60,26 +77,25 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to listings_url }
       format.json { head :no_content }
     end
   end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_listing
-    @listing = Listing.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def listing_params
-    params.require(:listing).permit(:name, :description, :price, :image)
-  end
-
-  def check_user
-    if current_user != @listing.user
-      redirect_to root_url, alert: 'Sorry, this listing belongs to someone else'
+    # Use callbacks to share common setup or constraints between actions.
+    def set_listing
+      @listing = Listing.find(params[:id])
     end
-  end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def listing_params
+      params.require(:listing).permit(:name, :description, :price, :image)
+    end
+
+    def check_user
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
+      end
+    end
 end
