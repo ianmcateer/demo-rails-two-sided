fbf62b35c4abbe7d371b5ff5230d0da1feeae107
ianmcateer <ian-mcateer55@hotmail.com>
2018-05-02 10:23:45 +1000

    created orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,6 @@ class Listing < ApplicationRecord
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
 
-  belongs_to :user
+  belongs_to :user, class_name: "User"
+  has_many :orders, class_name: "User"
 end
