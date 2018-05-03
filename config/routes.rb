08e71212fc9066c079b11a6b71f3449cb160ef99
ianmcateer <ian-mcateer55@hotmail.com>
2018-05-03 10:10:31 +1000

    modified listings view

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   resources :orders
   devise_for :users
   resources :listings do
-    resources :orders, only: [:new, :create]
+    resources :orders, only: %i[new create]
   end
 
   get 'pages/about'
   get 'pages/contact'
 
-  get 'seller' => "listings#seller"
+  get 'seller' => 'listings#seller'
 
-  get 'sales' => "orders#sales"
-    get 'purchases' => "orders#purchases"
+  get 'sales' => 'orders#sales'
+  get 'purchases' => 'orders#purchases'
 
   root 'listings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
