a19d8526dce3932635acb55c28f01e6f985c3ce3
ianmcateer <ian-mcateer55@hotmail.com>
2018-05-03 14:24:32 +1000

    fixed config file for cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = User.create([
+  { name: 'ianmcateer',
+    email: 'ian@ss',
+    password: '1234567'
+  },
+])
+
+
+listings = Listing.create([
+  { name: "Fender American Standard Stratocaster Electric Guitar",
+    description: "2006 Fender USA Stratocaster featuring slim maple neck with rosewood fingerboard, 3 tone sunburst alder body,
+     mint guard, and Fender Noiseless pickups. Original bridge modified with steel block saddles. Includes hard shell case.
+       Found Sound Australia Pty Ltd. 155 Elgin Street (Via Markov Place) Carlton, VIC 3053 Australia",
+    price:1748
+    image:
+    user_id:
+  }
+  ])
