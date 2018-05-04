80dc8cca2900704ab1d533ca2815cca8d7351649
ianmcateer <ian-mcateer55@hotmail.com>
2018-05-05 08:22:44 +1000

    converted from sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_03_214644) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.string "name"
     t.text "description"
