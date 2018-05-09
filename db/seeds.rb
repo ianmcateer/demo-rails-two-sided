429a087736a6372a381331a6dee99b52ea04a2e4
ianmcateer <ian-mcateer55@hotmail.com>
2018-05-09 11:38:48 +1000

    implemented stripe and views added more lisitngs in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,15 +95,56 @@ category.save!
 
 listings = Listing.create([
   { name: "Fender American Standard Stratocaster Electric Guitar",
-    description: "2006 Fender USA Stratocaster featuring slim maple neck with rosewood fingerboard, 3 tone sunburst alder body,
-     mint guard, and Fender Noiseless pickups. Original bridge modified with steel block saddles. Includes hard shell case.
-       Found Sound Australia Pty Ltd. 155 Elgin Street (Via Markov Place) Carlton, VIC 3053 Australia",
-    price:1748,
+    description: "Consolidating my gear. I am the only owner... bought this is Japan when I lived there in 2002. Hardly used... kept in case... really a beautiful guitar.",
+    price: 1098,
     image: Rails.root.join('app', 'assets', 'images', 'fender.png').open,
     user_id: 2,
     category_id: 1,
   }
   ])
+
+  listings = Listing.create([
+    { name: "PRS McCarty Hollowbody Spruce 2002 Natural",
+      description: "2006 Fender USA Stratocaster featuring slim maple neck with rosewood fingerboard, 3 tone sunburst alder body,
+       mint guard, and Fender Noiseless pickups. Original bridge modified with steel block saddles. Includes hard shell case.
+         Found Sound Australia Pty Ltd. 155 Elgin Street (Via Markov Place) Carlton, VIC 3053 Australia",
+      price:1748,
+      image: Rails.root.join('app', 'assets', 'images', 'electric_guitar_2.jpg').open,
+      user_id: 2,
+      category_id: 1,
+    }
+    ])
+
+  listings = Listing.create([
+    { name: "Kathleen Wingert E (Grand Concert) 2001 Spruce/Rosewood",
+      description: "Beautiful 2001 Kathy Wingert Model E Grand Concert in excellent condition.  Extraordinary tone with depth, warmth and great clarity and balance, and crazy-great harmonics.  Sitka spruce top, East Indian rosewood sides and back, koa binding, mahogany neck and ebony fingerboard.  Spectacular MOP inlay from 11th through 17th fret.  Beautiful scallop at the base of the headstock.  A truly spectacular work of art from one of the country's greatest and most celebrated luthiers.
+      With a Baggs piezo pickup under the saddle (with hidden soundhole volume control), and a custom Cedar Creek case.  Note: there are two very small dings near the base of the guitar as shown in the photo.  Otherwise, the instrument is flawless.  ",
+      price: 3748,
+      image: Rails.root.join('app', 'assets', 'images', 'acoustic_guitar_1.jpg').open,
+      user_id: 2,
+      category_id: 2,
+    }
+    ])
+
+  listings = Listing.create([
+    { name: "Martin D28 1974 Aged Natural Gloss",
+      description: "Absolutely pristine condition. Never saw my father play it in 35 years. Still has original plastic protective sticker on pick guard that is only peeling slightly (see picture). Shipping available. If you've been waiting for something special, this is it!  Make an offer.",
+      price: 1500,
+      image: Rails.root.join('app', 'assets', 'images', 'acoustic_guitar_2.jpg').open,
+      user_id: 2,
+      category_id: 2,
+    }
+    ])
+
+  listings = Listing.create([
+    { name: "Blackstar HT Club 40",
+      description: "With two channels and four modes, the Blackstar HT Club 40 is the perfect tube amp for gigging in small clubs without breaking the bank. Choose between the clean channel, which has boutique and modern modes for tons of tonal variation, or the overdrive channel, to get fully EQ-able, defined overdrive. The OD channel boasts two more modes: classic crunch and super saturated lead. So many options may be overwhelming, but the HT Club 40 is an intuitive, streamlined combo amp that just makes sense for the gigging musician.",
+      price: 900,
+      image: Rails.root.join('app', 'assets', 'images', 'amp_1.jpg').open,
+      user_id: 2,
+      category_id: 4,
+      }
+      ])
 
   mystore = Store.new
   mystore.title = "Sydney Centre"
